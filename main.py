a4e13d1160d2d8d7b960065764da175ceedbd03b
iamakhildabral <akhildabral02@gmail.com>
2023-06-01 22:16:31 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from googletrans import Translator
+
+
+window = ttk.Window(themename='darkly')
+window.title("Google Translator Desktop")
+window.geometry('600x500')
+
+def translate_text():
+    usertext = user_text.get()
+    translator = Translator()
+    translation = translator.translate(usertext, dest='hi')
+    translated_text_var.set(translation.text)
+
+# creating entrybox  for the user to translate
+
+user_text = ttk.Entry(master=window,textvariable="",font='Calibri 16')
+user_text.pack()
+
+translate_button = ttk.Button(master=window,text="Translate",command=translate_text)
+translate_button.pack()
+
+translated_text_var = tk.StringVar()
+translated_text = ttk.Label(master=window,textvariable=translated_text_var,font='Calibri 16')
+translated_text.pack()
+
+window.mainloop()
+
+
+
+
