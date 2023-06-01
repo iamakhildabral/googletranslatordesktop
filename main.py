11f06d494abf741c6b976a693b7e9fe690eb13e0
iamakhildabral <akhildabral02@gmail.com>
2023-06-01 23:56:16 +0530

    replaced the output label with scrolled text

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ window = ttk.Window(themename='darkly')
 window.title("Google Translator Desktop")
 window.geometry('800x650')
 
+# to maximize the window
+window.state('zoomed')
+
 
 def translate_text(event=None):
-    usertext = user_text.get()
+    usertext = user_text.get("1.0", tk.END)  # Get the content of the Text widget till end
     translator = Translator()
     translation = translator.translate(usertext, dest='hi')
-    translated_text_var.set(translation.text)
+    ##translated_text_var.set(translation.text)
+    scrolled_output_text.delete(1.0, tk.END)
+    scrolled_output_text.insert(tk.END, translation.text)
 
 
+# creating text box  for the user to translate
 
-# creating entrybox  for the user to translate
-
-user_text = ttk.Entry(master=window, textvariable="", font='Calibri 16',width=35)
-user_text.pack(pady=50,padx=10)
-
+user_text = ttk.Text(master=window, font='Calibri 16', height=8, width=70, wrap=ttk.WORD)
+user_text.pack(pady=50, padx=10)
 
 translate_button = ttk.Button(master=window, text="Translate", command=translate_text)
 translate_button.pack(pady=30)
@@ -27,12 +30,9 @@ translate_button.pack(pady=30)
 window.bind('<Return>', translate_text)
 
 
-output_label = ttk.Label(master=window, text="Translated Text",font='Calibri 14')
-output_label.pack(pady=20)
+scrolled_output_text = ttk.ScrolledText(master=window,font="Calibri 14")
+scrolled_output_text.pack(expand=True,pady=60)
 
-# showing the converted value
-translated_text_var = tk.StringVar()
-translated_text = ttk.Label(master=window,textvariable=translated_text_var, font='Calibri 16',)
-translated_text.pack(pady=30)
+
 
 window.mainloop()
