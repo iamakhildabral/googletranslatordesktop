1d7c8d722d79ae1d303f9415ab34274a399308fe
iamakhildabral <akhildabral02@gmail.com>
2023-06-01 22:48:00 +0530

    added return command

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ window.title("Google Translator Desktop")
 window.geometry('600x500')
 
 
-def translate_text():
+def translate_text(event=None):
     usertext = user_text.get()
     translator = Translator()
     translation = translator.translate(usertext, dest='hi')
@@ -22,8 +22,15 @@ user_text.pack()
 translate_button = ttk.Button(master=window, text="Translate", command=translate_text)
 translate_button.pack()
 
+window.bind('<Return>', translate_text)
+
+
+output_label = ttk.Label(master=window, text="Translated Text",font='Calibri 14')
+output_label.pack()
+
+# showing the converted value
 translated_text_var = tk.StringVar()
-translated_text = ttk.Label(master=window, text="your translated text will come here",textvariable=translated_text_var, font='Calibri 16',)
+translated_text = ttk.Label(master=window,textvariable=translated_text_var, font='Calibri 16',)
 translated_text.pack()
 
 window.mainloop()
