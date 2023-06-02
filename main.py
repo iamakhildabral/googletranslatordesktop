d784049959ce67682924090bd35107fecc19cd8b
iamakhildabral <akhildabral02@gmail.com>
2023-06-02 19:41:52 +0530

    added transalte button disabled status white /n transation/n also tried to set some style in the scrolled text widget and added a execption tracking for big content translation failure

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import tkinter as tk
+from tkinter import messagebox
+
 import ttkbootstrap as ttk
 from googletrans import Translator
 
 window = ttk.Window(themename='darkly')
 window.title("Google Translator Desktop")
 window.geometry('800x650')
-
 # to maximize the window
 window.state('zoomed')
 
 
 def translate_text(event=None):
-    usertext = user_text.get("1.0", tk.END)  # Get the content of the Text widget till end
-    translator = Translator()
-    translation = translator.translate(usertext, dest='hi')
-    ##translated_text_var.set(translation.text)
-    scrolled_output_text.delete(1.0, tk.END)
-    scrolled_output_text.insert(tk.END, translation.text)
+    translate_button.config(state=tk.DISABLED)
+    user_entered_text = user_text.get("1.0", tk.END)  # Get the content of the Text widget till end
+    try:
+        translator = Translator()
+        translation = translator.translate(user_entered_text, dest='hi')
 
+        # Update the scrolled text widget with the translated text
+        scrolled_output_text.delete(1.0, tk.END)
+        scrolled_output_text.insert(tk.END, translation.text)
+    except Exception as e:
+        messagebox.showerror("Error", "Translation failed. Please check your internet connection.")
+
+
+    # Enable the Translate button after translation is completed
+    translate_button.config(state=tk.NORMAL)
 
 # creating text box  for the user to translate
 
@@ -29,10 +38,18 @@ translate_button.pack(pady=30)
 
 window.bind('<Return>', translate_text)
 
+# creating scrolled text for the translated output
+scrolled_output_text = ttk.ScrolledText(master=window, font="Calibri 14")
+scrolled_output_text.pack(expand=True, pady=60)
 
-scrolled_output_text = ttk.ScrolledText(master=window,font="Calibri 14")
-scrolled_output_text.pack(expand=True,pady=60)
+# Set the scrollbar colors to match the 'darkly' theme
+style = ttk.Style()
+style.theme_use("darkly")
+style.configure("Vertical.TScrollbar", troughcolor=style.lookup("TFrame", "background"),
+                background=style.lookup("TFrame", "background"), darkcolor=style.lookup("TFrame", "background"),
+                lightcolor=style.lookup("TFrame", "background"))
 
-
+# Apply the style to the scrolled text widget
+scrolled_output_text.config(yscrollcommand=style.configure("Vertical.TScrollbar"))
 
 window.mainloop()
